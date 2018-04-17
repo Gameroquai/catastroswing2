4ae8c25801b8a2c1a1fab074def21e464f7be9bd
LuisMGG <guerrero.luismiguel@gmail.com>
2018-04-17 14:09:26 +0200

    Ejercicios plsql

---
+++ BBDD/PLSQL/6_EJERCICIO_PROPUESTO_PLSQL.sql
@@ -0,0 +1,13 @@
+/*Desarrolla una función que devuelva el número de años completos
+ que hay entre dos fechas que se pasan como parámetros.  */
+
+CREATE OR REPLACE FUNCTION dif_anios (
+data1 DATE, data2 DATE)
+RETURN NUMBER
+AS
+	v_anys_dif NUMBER(6);
+BEGIN
+	v_anys_dif := ABS(TRUNC(MONTHS_BETWEEN(data2,data1)/ 12));
+	RETURN v_anys_dif;
+END dif_anios;
+/
